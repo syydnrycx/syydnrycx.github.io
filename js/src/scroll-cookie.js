0cee9ee681bc0a776ab9b81738d457b163a93254
syydnrycx <cx1307349986@gmail.com>
2019-07-04 21:58:53 +0800

    Site updated: 2019-07-04 21:58:32

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Sat Dec 29 2018 23:19:53 GMT+0800 (中国标准时间)
+// build time:Thu Jul 04 2019 21:58:25 GMT+0800 (中国标准时间)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 

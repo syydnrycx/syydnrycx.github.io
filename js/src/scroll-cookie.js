6d2858aec994efd1c83a8e2125db3eeafb87e1ce
syydnrycx <cx1307349986@gmail.com>
2020-11-20 19:37:05 +0800

    add config url

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Fri Nov 22 2019 16:42:07 GMT+0800 (中国标准时间)
+// build time:Fri Nov 20 2020 19:36:13 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
